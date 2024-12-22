c80b5d5904a8427b33ab07b8248d753c6360a262
FinnTew <nanomoa76@gmail.com>
2024-12-22 09:05:32 +0800

    update

--- ResumeParserBackend/Program.cs
+++ ResumeParserBackend/Program.cs
@@ -1,13 +1,14 @@
-using Microsoft.AspNetCore.Routing.Constraints;
-using Microsoft.Extensions.Logging.EventSource;
+using FluentScheduler;
 using MongoDB.Bson;
 using ResumeParserBackend.Util;
-using NodaTime;
 using ResumeParserBackend;
 using ResumeParserBackend.Collection;
 using ResumeParserBackend.Document;
 using ResumeParserBackend.Entity;
 using ResumeParserBackend.Helper;
+
+// ElasticSearch 定时刷新任务
+JobManager.Initialize(new ElasticSearchTimer());
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -103,16 +104,64 @@ app.MapPost("/upload", async (HttpContext ctx) =>
     .WithName("UploadFile")
     .WithOpenApi();
 
+// 获取所有简历
+app.MapGet("/list", async (HttpContext ctx) =>
+    {
+        var mongo = new MongoDbHelper<Resume>("Resume");
+
+        var resumeList = await mongo.FindAllAsync();
+
+        var resumeListRes = new List<ResumeEntity>();
+        resumeList.ForEach(item =>
+        {
+            resumeListRes.Add(new ResumeEntity
+            {
+                ResumeId = item.ResumeId,
+                OriginFileName = item.OriginalFileName,
+                UploadTime = item.UploadTime,
+                ParseStatus = item.ParseStatus
+            });
+        });
+        
+        return Results.Json(new {success = true, resumeList = resumeListRes}, statusCode: StatusCodes.Status200OK);
+    })
+    .WithName("List")
+    .WithOpenApi();
+
+// 文件内容
+app.MapGet("filecontent/{resumeId}", async (string resumeId, HttpContext ctx) =>
+    {
+        var mongo = new MongoDbHelper<ResumeContent>("ResumeContent");
+        
+        var resumeContent = await mongo.FindOneAsync(f => f.ResumeId == resumeId);
+        
+        return Results.Json(new {success = true, fileContent = resumeContent.FileContent}, statusCode: StatusCodes.Status200OK);
+    })
+    .WithName("GetFileContent")
+    .WithOpenApi();
+
 // 按照文件名查找
 app.MapGet("/find/{fileName}", async (string fileName, HttpContext ctx) =>
     {
         var mongo = new MongoDbHelper<Resume>("Resume");
 
         var resumeList = await mongo.FindManyAsync(f => f.OriginalFileName.Contains(fileName));
+        
+        var resumeListRes = new List<ResumeEntity>();
+        resumeList.ForEach(item =>
+        {
+            resumeListRes.Add(new ResumeEntity
+            {
+                ResumeId = item.ResumeId,
+                OriginFileName = item.OriginalFileName,
+                UploadTime = item.UploadTime,
+                ParseStatus = item.ParseStatus
+            });
+        });
 
-        return Results.Json(new { success = true, resumeList }, statusCode: StatusCodes.Status200OK);
+        return Results.Json(new { success = true, resumeList = resumeListRes }, statusCode: StatusCodes.Status200OK);
     })
-    .WithName("Find")
+    .WithName("FindByFileName")
     .WithOpenApi();
 
 // 按照日期查找
@@ -134,9 +183,21 @@ app.MapPost("/find", async (HttpContext ctx) =>
         
         var resumeList = await mongo.FindManyAsync(f => f.UploadTime >= reqData.StartDate && f.UploadTime <= reqData.EndDate);
         
-        return Results.Json(new { success = true, resumeList }, statusCode: StatusCodes.Status200OK);
+        var resumeListRes = new List<ResumeEntity>();
+        resumeList.ForEach(item =>
+        {
+            resumeListRes.Add(new ResumeEntity
+            {
+                ResumeId = item.ResumeId,
+                OriginFileName = item.OriginalFileName,
+                UploadTime = item.UploadTime,
+                ParseStatus = item.ParseStatus
+            });
+        });
+        
+        return Results.Json(new { success = true, resumeList = resumeListRes }, statusCode: StatusCodes.Status200OK);
     })
-    .WithName("Find")
+    .WithName("FindByDate")
     .WithOpenApi();
 
 // 下载简历
@@ -160,6 +221,30 @@ app.MapGet("/download/{resumeId}", async (string resumeId, HttpContext ctx) =>
     })
     .WithName("Download")
     .WithOpenApi();
+
+// 结构化数据获取
+app.MapGet("/structure/{resumeId}", async (string resumeId, HttpContext ctx) =>
+    {
+        var r = await new MongoDbHelper<Resume>("Resume").FindOneAsync(f => f.ResumeId == resumeId);
+        if (r.ParseStatus != "success")
+        {
+            return Results.Json(new { success = false, message = "No parsed resume." }, statusCode: StatusCodes.Status400BadRequest);
+        }
+        
+        var mongo = new MongoDbHelper<ResumeMetadata>("ResumeMetadata");
+        
+        var resume = await mongo.FindOneAsync(f => f.ResumeId == resumeId);
+
+        var jsonString = resume.Metadata.ToJson();
+        
+        // TODO: Convert to other type
+        
+        return Results.Json(new { success = true, data = new {} }, statusCode: StatusCodes.Status200OK);
+    })
+    .WithName("Structure")
+    .WithOpenApi();
+
+
 
 // 关键词匹配
 app.MapPost("/match", async (HttpContext ctx) =>
@@ -220,9 +305,34 @@ app.MapPost("/match", async (HttpContext ctx) =>
         return Results.Json(new {success = true, matchResList}, statusCode: StatusCodes.Status200OK);
     })
     .Accepts<MatchReq>("application/json")
-    .WithName("Match")
+    .WithName("MatchByKeywords")
     .WithOpenApi();
 
-
+// 语义匹配
+app.MapGet("/match/{jobId}", async (string jobId, HttpContext ctx) =>
+    {
+        var job = await new MongoDbHelper<Job>("Job").FindOneAsync(f => f.JobId == jobId);
+        if (job.ParseStatus != "success")
+        {
+            return Results.Json(new { success = false, message = "No parsed job." }, statusCode: StatusCodes.Status400BadRequest);
+        }
+        var jobMetadata = await new MongoDbHelper<JobMetadata>("JobMetadata").FindOneAsync(f => f.JobId == jobId);
+        var jobJson = jobMetadata.Metadata.ToJson();
+        
+        var resumeMetaList = await new MongoDbHelper<ResumeMetadata>("ResumeMetadata").FindAllAsync();
+        
+        resumeMetaList.ForEach(resume =>
+        {
+            var resumeJson = resume.Metadata.ToJson();
+            
+            // TODO: 人岗匹配
+            
+            // TODO: 持久化匹配结果
+        });
+        
+        return Results.Json(new {success = true, message = "Match success."}, statusCode: StatusCodes.Status200OK);
+    })
+    .WithName("MatchByJobId")
+    .WithOpenApi();
 
 app.Run();
